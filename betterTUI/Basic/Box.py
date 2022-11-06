b2507b068f62280f925eb902b6df78580415f561
Oscar Gomez <56361339+Gomez0015@users.noreply.github.com>
2022-11-07 00:08:39 +0100

    Add files via upload

--- betterTUI/Basic/Box.py
+++ betterTUI/Basic/Box.py
@@ -25,9 +25,9 @@ class Box:
         screen.addstr(y+height, x+width, '┘', *args)
 
     def clear(self):
-        for i in range(3, self.width-1):
-            for x in range(1, self.height):
-                self.screen.addstr(self.y+x, i, " ")
+        for i in range(1, self.width-1):
+            for x in range(1, self.height-1):
+                self.screen.addstr(self.y+x, self.x+i, " ")
 
     def delete(self):
         for i in range(0, self.width+1):
